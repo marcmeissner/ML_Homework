7b5652b2e7a1f06a568762c937651dd7a7c4737e
Marc Meissner <marcmeissner@hotmail.de>
2017-10-25 15:26:44 +0200

    add exercise 4

---
+++ ML_Homework_01/decision_tree_ex1.py
@@ -0,0 +1,123 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from csv import reader
+
+def load_csv(filename):
+    file = open(filename, "rb")
+    lines = reader(file)
+    dataset = list(lines)
+    return dataset
+
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+
+# Split a dataset based on an attribute and an attribute value
+def test_split(index, value, dataset):
+    left, right = list(), list()
+    for row in dataset:
+        if row[index] < value:
+            left.append(row)
+        else:
+            right.append(row)
+    return left, right
+
+
+# Calculate the Gini index for a split dataset
+def gini_index(groups, classes):
+    # count all samples at split point
+    n_instances = float(sum([len(group) for group in groups]))
+    # sum weighted Gini index for each group
+    gini = 0.0
+    for group in groups:
+        size = float(len(group))
+        # avoid divide by zero
+        if size == 0:
+            continue
+        score = 0.0
+        # score the group based on the score for each class
+        for class_val in classes:
+            p = [row[-1] for row in group].count(class_val) / size
+            score += p * p
+        # weight the group score by its relative size
+        gini += (1.0 - score) * (size / n_instances)
+    return gini
+
+
+# Select the best split point for a dataset
+def get_split(dataset):
+    class_values = list(set(row[-1] for row in dataset))
+    b_index, b_value, b_score, b_groups = 999, 999, 999, None
+    for index in range(len(dataset[0]) - 1):
+        for row in dataset:
+            groups = test_split(index, row[index], dataset)
+            gini = gini_index(groups, class_values)
+            if gini < b_score:
+                b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+    return {'index': b_index, 'value': b_value, 'groups': b_groups}
+
+
+# Create a terminal node value
+def to_terminal(group):
+    outcomes = [row[-1] for row in group]
+    return max(set(outcomes), key=outcomes.count)
+
+
+# Create child splits for a node or make terminal
+def split(node, max_depth, min_size, depth):
+    left, right = node['groups']
+    del (node['groups'])
+    # check for a no split
+    if not left or not right:
+        node['left'] = node['right'] = to_terminal(left + right)
+        return
+    # check for max depth
+    if depth >= max_depth:
+        node['left'], node['right'] = to_terminal(left), to_terminal(right)
+        return
+    # process left child
+    if len(left) <= min_size:
+        node['left'] = to_terminal(left)
+    else:
+        node['left'] = get_split(left)
+        split(node['left'], max_depth, min_size, depth + 1)
+    # process right child
+    if len(right) <= min_size:
+        node['right'] = to_terminal(right)
+    else:
+        node['right'] = get_split(right)
+        split(node['right'], max_depth, min_size, depth + 1)
+
+
+# Build a decision tree
+def build_tree(train, max_depth, min_size):
+    root = get_split(train)
+    split(root, max_depth, min_size, 1)
+    return root
+
+
+# Make a prediction with a decision tree
+def predict(node, row):
+    if row[node['index']] < node['value']:
+        if isinstance(node['left'], dict):
+            return predict(node['left'], row)
+        else:
+            return node['left']
+    else:
+        if isinstance(node['right'], dict):
+            return predict(node['right'], row)
+        else:
+            return node['right']
+
+
+# Classification and Regression Tree Algorithm
+def decision_tree(train, test, max_depth, min_size):
+    tree = build_tree(train, max_depth, min_size)
+    predictions = list()
+    for row in test:
+        prediction = predict(tree, row)
+        predictions.append(prediction)
+    return (predictions)
